6c9adab7604225a10030d48eeec312f30c217981
Gustavo <gustavolopez819@gmail.com>
2026-05-15 14:04:18 -0500

    feat(metadata): added generatePageTitle fn to root layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { GeneratePageTitle } from "@/src/shared/utils/metadata";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -9,7 +10,7 @@ const outfit = Outfit({
 
 
 export const metadata: Metadata = {
-  title: "Meeti - Next",
+  title: GeneratePageTitle('Next'),
   description: "Proyecto Meeti Next.js on DrizzleORM",
 };
 
