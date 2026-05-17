aff893a7727e6a2a5eddfeafb78313e6924783b2
Gustavo <gustavolopez819@gmail.com>
2026-05-16 23:36:49 -0500

    feat(register-form): added react hook form

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,7 +1,8 @@
 import { Form, FormInput, FormLabel, FormSubmit } from "@/components/forms";
+import { useForm } from "react-hook-form";
 
 export default function RegisterForm() {
-
+  const { register, handleSubmit, watch, formState: { errors }} = useForm();
   return (
     <Form>
       <FormLabel htmlFor="name">Nombre</FormLabel>
@@ -9,12 +10,14 @@ export default function RegisterForm() {
         id="name"
         type="text"
         placeholder="Ingresa tu Nombre"
+        {...register('name')}
       />
       <FormLabel htmlFor="email">E-mail</FormLabel>
       <FormInput
         id="email"
         type="email"
         placeholder="Ingresa tu E-mail"
+        {...register('email')}
       />
 
       <FormLabel htmlFor="password">Password</FormLabel>
@@ -22,12 +25,14 @@ export default function RegisterForm() {
         id="password"
         type="password"
         placeholder="Password - Min 8 Caracteres"
+        {...register('password')}
       />
       <FormLabel htmlFor="password_confirmation">Repetir Password</FormLabel>
       <FormInput
         id="password_confirmation"
         type="password"
         placeholder="Repite tu Password"
+         {...register('passwordConfirmation')}
       />
 
       <FormSubmit value='Registrarme' />
