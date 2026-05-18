f440d9649cb01254d6903967a67ac7dad9d78540
Gustavo <gustavolopez819@gmail.com>
2026-05-18 14:09:46 -0500

    feat(auth-schema): added refine method comparing password with password confirmation

--- src/features/auth/schemas/authSchema.ts
+++ src/features/auth/schemas/authSchema.ts
@@ -12,4 +12,7 @@ export const SignUpSchema = BaseAuthSchema.pick({
   email: true,
   password: true,
   passwordConfirmation: true
+}).refine( data => data.password === data.passwordConfirmation, {
+  error: "Los Passwords no son iguales",
+  path: ['passwordConfirmation']
 })
