aba71d05c7d0a45bd693c1dc3bacd6b96ba9f11a
Gustavo <gustavolopez819@gmail.com>
2026-05-22 22:07:08 -0500

    feat(schema): add auth schema with users, sessions, accounts, and verifications tables

---
+++ src/db/schema/index.ts
@@ -0,0 +1,10 @@
+import { users, sessions, accounts, verifications } from './auth'
+import { comunity } from './comunity'
+
+export {
+  users, 
+  sessions,
+  accounts,
+  verifications,
+  comunity
+}
