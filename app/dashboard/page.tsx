d66aee6d7b4e931deecb068b3545784a724b9c36
Gustavo <gustavolopez819@gmail.com>
2026-05-25 13:41:42 -0500

    feat(dashboard): update DashboardPage to include authentication check and heading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,15 @@
-export default function DashboardPage() {
+import { requiredAuth } from "@/src/lib/auth-server";
+import Heading from "@/src/shared/components/typography/Heading";
+import { redirect } from "next/navigation";
 
+export default async function DashboardPage() {
+
+  const { isAuth } = await requiredAuth();
+  if (isAuth) redirect('/auth/login');
+  
   return (
-    <div>dashboard</div>
+    <>
+    <Heading>Panel de Administración</Heading>
+    </>
   )
 }
