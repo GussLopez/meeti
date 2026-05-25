c2df4d734c9ee6bda5d876693f1922fc8504bf90
Gustavo <gustavolopez819@gmail.com>
2026-05-25 00:39:36 -0500

    feat(auth): add login method to AuthService for user authentication

--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,21 +1,21 @@
 import { auth } from "@/lib/auth";
-import { SignUpInput } from "../schemas/authSchema";
+import { SignInInput, SignUpInput } from "../schemas/authSchema";
 import { authRepository, IAuthRepository } from "./AuthRepository";
+import { headers } from "next/headers";
+import { APIError } from "better-auth";
 
 class AuthService {
-  constructor (
-    private authRepository : IAuthRepository
-  ){}
-  
+  constructor(private authRepository: IAuthRepository) {}
+
   async register(credentials: SignUpInput) {
     const { name, email, password } = credentials;
 
     const user = await this.authRepository.userExist(email);
     if (user) {
       return {
-        error: 'Este e-mail ya esta registrado',
-        success: ''
-      }
+        error: "Este e-mail ya esta registrado",
+        success: "",
+      };
     }
     await auth.api.signUpEmail({
       body: {
@@ -28,6 +28,53 @@ class AuthService {
     return {
       error: "",
       success: "Cuenta creada correctamente, revisa tu e-mail",
+    };
+  }
+
+  async login(credentials: SignInInput) {
+    const { email, password } = credentials;
+
+    const user = await this.authRepository.userExist(email);
+    if (!user) {
+      return {
+        error: "Este usuario no existe",
+        success: "",
+      };
+    }
+
+    try {
+      await auth.api.signInEmail({
+        body: {
+          email,
+          password,
+          callbackURL: '/dashboard'
+        },
+        headers: await headers()
+      });
+
+      return {
+        error: '',
+        success: 'Sesión iniciada correctamente'
+      }
+    } catch (error) {
+      if (error instanceof APIError) {
+        const messages: Record<number, string> = {
+          401: 'Password Incorrecto'
+        };
+
+        const errorMessage = messages[error.statusCode];
+        if (errorMessage) {
+          return {
+            error: errorMessage,
+            success: ''
+          }
+        }
+      }
+    }
+
+    return {
+      error: "",
+      success: "",
     };
   }
 }
