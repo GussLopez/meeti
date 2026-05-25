961ec9cc28ab6851edc1c83e5726f01c8ef6e14b
Gustavo <gustavolopez819@gmail.com>
2026-05-25 13:28:22 -0500

    feat(auth): add session retrieval in Home component

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,12 +1,19 @@
 import Hero from "@/components/ui/Hero";
+import { auth } from "@/lib/auth";
 import { GeneratePageTitle } from "@/src/shared/utils/metadata";
 import { Metadata } from "next";
+import { headers } from "next/headers";
 
 export const metadata: Metadata = {
   title: GeneratePageTitle('Inicio')
   
 }
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  console.log(session);
   return (
     <>
       <Hero />
