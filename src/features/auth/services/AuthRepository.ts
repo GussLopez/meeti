dc700e51bd5b4162dd81a90735641e3cdcc98a02
Gustavo <gustavolopez819@gmail.com>
2026-05-24 19:52:37 -0500

    feat(auth): define IAuthRepository interface and implement userExist method

--- src/features/auth/services/AuthRepository.ts
+++ src/features/auth/services/AuthRepository.ts
@@ -1,6 +1,18 @@
+import { db } from "@/src/db";
+import { User } from "../types/auth.types";
 
-class AuthRepository {
+export interface IAuthRepository {
+  userExist(email: string): Promise<User | undefined>
+}
 
+class AuthRepository implements IAuthRepository {
+  async userExist(email: string) {
+    return await db.query.users.findFirst({
+      where: {
+        email,
+      },
+    });
+  }
 }
 
 export const authRepository = new AuthRepository();
