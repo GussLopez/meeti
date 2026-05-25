683bada3f9e610bdb51d62acde3bcbd357c86668
Gustavo <gustavolopez819@gmail.com>
2026-05-25 13:41:57 -0500

    feat(session): removed session

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,19 +1,13 @@
 import Hero from "@/components/ui/Hero";
-import { auth } from "@/lib/auth";
 import { GeneratePageTitle } from "@/src/shared/utils/metadata";
 import { Metadata } from "next";
-import { headers } from "next/headers";
 
 export const metadata: Metadata = {
   title: GeneratePageTitle('Inicio')
   
 }
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  })
-
-  console.log(session);
+ 
   return (
     <>
       <Hero />
